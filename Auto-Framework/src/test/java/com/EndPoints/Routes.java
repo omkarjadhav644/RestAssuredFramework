8325e9048cc7da550e036c0ccb3a0b197550c4c2
Omkar Jadhav <omkarjadhav@Omkars-MacBook-Air.local>
2025-09-12 12:43:23 +0530

    User API test cases

---
+++ Auto-Framework/src/test/java/com/EndPoints/Routes.java
@@ -0,0 +1,13 @@
+package com.EndPoints;
+
+public class Routes {
+
+    public static String base_url= "https://petstore.swagger.io/v2";
+
+    //User module API endpoints
+
+    public static String user_post_url=base_url+"/user";
+    public static String user_get_url=base_url+"/user/{username}";
+    public static String user_update_url=base_url+"/user/{username}";
+    public static String user_delete_url=base_url+"/user/{username}";
+}
